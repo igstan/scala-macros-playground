6a6b4fde02848fd71561269668a5b363a2340eff
Ionuț G. Stan <ionut.g.stan@gmail.com>
2013-05-28 14:58:40 +0300

    Replace manual AST construction with quasiquotes

--- macros/src/scala/StringEnum.scala
+++ macros/src/scala/StringEnum.scala
@@ -7,39 +7,30 @@ object StringEnum {
   def impl[A](c: Context)(factory: c.Expr[String => A], vals: c.Expr[String]*) = {
     import c.universe._
 
-    // A `Template` is whatever comes after the `extends` keyword when you
-    // declare a Scala class or object. Here we're getting the Template of
-    //
-    //   object Ramones extends Enum("Joey", "Johnny", "Tommy")
-    //
-    // A Template consists of:
-    //
-    //  - a list of parents of that class/object
-    //  - a representation of the self type (not sure what this is yet)
-    //  - the body (constructor + definitions) of the class/object
-    //
     val Template(parents, self, ctor :: defs) = c.enclosingTemplate
 
-    // Now, we're just mapping over the list of names we've passed to `Enum`
-    // in order to transform them into `val` definitions.
     val values = vals.map { value =>
-      // Each name is the AST representation of a String, so we have to
-      // extract the actual string name from it. In this case we're using
-      // pattern matching in assignment.
       val Expr(Literal(Constant(name: String))) = value
       val fn = c.resetAllAttrs(factory.tree)
 
-      // Finally, we're building the AST for the `val` definition
-      ValDef(
-        mods = Modifiers(),
-        name = TermName(name),
-        tpt  = TypeTree(),
-        rhs  = Apply(fn, List(Literal(Constant(name))))
-      )
+      // Manual AST construction mixed with quasiquotes, which works fine.
+      //
+      // ValDef(
+      //   mods = Modifiers(),
+      //   name = TermName(name),
+      //   tpt  = TypeTree(),
+      //   rhs  = q"$fn($name)"
+      // )
+
+      // Building the AST for the `val` definition using quasiquotation.
+
+      // Not working
+      // q"val $name = $fn($name)"
+
+      // Working
+      q"val ${TermName(name)} = $fn($name)"
     }
 
-    // Having a list of `val` ASTs, we can now build a new Template to
-    // replace the one given to us by the compiler.
     Template(Nil, emptyValDef, ctor :: defs ++ values)
   }
 
