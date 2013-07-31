4cfb7f2a24e1b50b868df7dfe57f41a783c516f6
Ionuț G. Stan <ionut.g.stan@gmail.com>
2013-07-31 15:46:54 +0300

    An identity macro, useful for exploring ASTs

--- core/src/scala/Main.scala
+++ core/src/scala/Main.scala
@@ -1,35 +1,11 @@
 package ro.igstan.core
 
-import ro.igstan.macros.Macros.describe
+import ro.igstan.macros.Macros._
 
 object Main {
-
-  case class Property(id: String, foo: String, bar: String)
-
-  trait BSONDocumentWriter[P]
-
-  implicit object propertyBSONWriter extends BSONDocumentWriter[Property] {
-    def write(property: Property) = {
-      Seq()                      ++
-      Seq("ID"  -> property.id)  ++
-      Seq("FOO" -> property.foo) ++
-      Seq("BAR" -> property.bar)
-    }
-  }
-
-  val BSONDocument = Seq
-
-  implicit object propertyBSONWriter2 extends BSONDocumentWriter[Property] {
-    def write(property: Property) = describe(property)
-  }
-
   def main(args: Array[String]): Unit = {
-    val p = Property("id", "123", "456")
-
-    val w1 = propertyBSONWriter.write(p)
-    println(w1)
-
-    val w2 = propertyBSONWriter2.write(p)
-    println(w2)
+    id {
+      List(1, 2, 3)
+    }
   }
 }
